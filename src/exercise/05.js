b895b270843b97296d74d72b5506c44a67d3b5b3
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2021-09-17 18:01:01 +0100

    Finish React-fundamental module, lessons 5 through 7

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,9 +12,25 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+
+// const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}> small lightblue box </div>
+// const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'italic'}}> medium pink box </div>
+// const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}> large orange box</div>
+
+// 1. 💯 Create a custom component
+// 2. 💯 accept a size prop to encapsulate styling
+
+const Box = ({size, className = '', style = {}, ...props}) => {
+  const boxSize = size ? `box--${size}` : ''
+  const classes = `box ${boxSize} ${className}`
+  const styles = {fontStyle: 'italic', ...style}
+  return <div className={classes} style={styles} {...props} />
+}
+
+const smallBox = <Box size="small" style={{backgroundColor: 'lightblue'}}> small lightblue box </Box>
+const mediumBox = <Box size="medium" style={{backgroundColor: 'pink'}}> medium pink box </Box>
+const largeBox = <Box size="large" style={{backgroundColor: 'orange'}}> large orange box</Box>
+
 
 function App() {
   return (
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,29 +3,84 @@
 
 import * as React from 'react'
 
-function UsernameForm({onSubmitUsername}) {
+const UsernameForm = ({onSubmitUsername}) => {
   // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
+  // 💰 Accept the `event` as an argument and call `event.preventDefault()` to prevent the default behaviors
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
+  // 🐨 Get the value from the username input (using whichever method you prefer from the options mentioned in instructions)
   // 🐨 Call `onSubmitUsername` with the value of the input
+  // 🐨 Add the onSubmit handler to the <form> below
+  // 🐨 Make sure to associate the label to the input to do so, set the value of 'htmlFor' prop of the label to the id of input
 
-  // 🐨 add the onSubmit handler to the <form> below
+  // const inputLabelEl = <label htmlFor="username">Username:</label>
+  // const inputEl = <input id="username" type="text" />
+  // const handleSubmit = event => {
+  //   event.preventDefault()
+  //   onSubmitUsername(event.target.elements.username.value)
+  // }
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  // 1. 💯 access the username value using react ref objects
+  // const usernameRef = React.useRef('');
+  // const inputLabelEl = <label htmlFor="username">Username:</label>
+  // const inputEl = <input type="text" id="username" ref={usernameRef} />
+  // const handleSubmit = (event) => {
+  //   event.preventDefault()
+  //   onSubmitUsername(usernameRef.current.value)
+  // }
+
+  // 2.💯 Validate lower-case
+  // const handleSubmit = event => {
+  //   event.preventDefault()
+  //   onSubmitUsername(event.target.elements.username.value)
+  // }
+  //
+  // const handleChange = event => {
+  //   const inputValue = event.target.value
+  //   const validValue = inputValue === inputValue.toLowerCase()
+  //   setErrorMessage(
+  //     validValue ? '' : 'Username must contain only lower case characters.',
+  //   )
+  // }
+  //
+  // const [error, setError] = React.useState('')
+  //
+  // const inputLabelEl = <label htmlFor="username">Username:</label>
+  // const inputEl = <input type="text" id="username" onChange={handleChange} />
+  //
+  // const setErrorMessage = value => setError(value)
+
+  // 3. 💯 Control the input value (like in Vue js using the value={something})
+  const handleSubmit = event => {
+    event.preventDefault()
+    onSubmitUsername(sanitizedInputValue)
+  }
+
+  const handleChange = event => {
+    sanitizeInputValue(event.target.value)
+  }
+
+
+  const [sanitizedInputValue, setInputValue] = React.useState('')
+  
+  const inputLabelEl = <label htmlFor="username">Username:</label>
+  const inputEl = <input type="text" id="username" value={sanitizedInputValue} onChange={handleChange} />
+  
+  const sanitizeInputValue = value => setInputValue(value.toLowerCase())
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        {inputLabelEl}
+        {inputEl}
       </div>
       <button type="submit">Submit</button>
+      {/* 
+      Use snippet below to use with exercise (1) and (3)
+        <button type="submit">Submit</button>
+      Use snippet below to use with exercise (2)
+        <p style={{color: 'red'}}>error</p>
+        <button type="submit" disabled={Boolean(error)}>Submit</button>
+      */}
     </form>
   )
 }
